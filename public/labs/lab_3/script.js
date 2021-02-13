08fc15a6ff56d80365d8d5b74870c45fd3483cfb
jmcint21 <50252036+jmcint21@users.noreply.github.com>
2021-02-12 21:01:22 -0500

    updated JS

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -17,17 +17,13 @@ let i = 1;
     let position = 0;
 
     carousel.querySelector('.prev').onclick = function() {
-     
       position += width * count;
-      
       position = Math.min(position, 0)
       list.style.marginLeft = position + 'px';
     };
 
-    carousel.querySelector('.next').onclick = function() {
-      
+    carousel.querySelector('.next').onclick = function() { 
       position -= width * count;
-      
       position = Math.max(position, -width * (listElems.length - count));
       list.style.marginLeft = position + 'px';
     };
